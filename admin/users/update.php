42651be0940f062c40abf0640e88917c28d634d5
riskykrnawan <riskykrnawan@gmail.com>
2022-11-08 20:04:51 +0800

    Fix: Bug Upload

--- admin/users/update.php
+++ admin/users/update.php
@@ -1,21 +1,20 @@
 <?php
   session_start();
+  require '../../config.php';
   if (!isset($_SESSION["loginAdmin"])) {
     header('Location: login.php');
     exit;
   }
-  require 'config.php';
   $id = $_GET["id"];
   $user = query("SELECT * FROM users WHERE id = $id")[0];
   if (isset($_POST["submitupdate"])) {
-    if (updateUser($_POST) > 0) echo "<script> alert('Data updated succesfully.'); document.location.href = 'dashboard.php'; </script>";
+    if (updateUser($_POST) > 0) echo "<script> alert('Data updated succesfully.'); document.location.href = 'index.php'; </script>";
     else {
       $feedback = "Data Gagal Diubah";
       echo mysqli_error($db);
     }
   }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,7 +24,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
   <title>Update Profile</title>
-  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="../../resources/css/login.css">
 </head>
 
 <body>
@@ -37,11 +36,10 @@
           <div class="input-field-photo">
             <table>
               <td>
-                <img src="img/<?= $user["photo"] ?>" alt="Your photo" width="100">
+                <img src="../../resources/img/<?= $user["photo"] ?>" alt="Your photo" width="100">
               </td>
               <td>
                 <input type="file" name="photo" placeholder="Add Picture...">
-                <input type="hidden" name="id" value="<?= $user['id'] ?>">
 
                 <input type="hidden" name="samePhoto" value="<?= $user["photo"] ?>"><br>
                 <input type="hidden" name="id" value="<?= $user['id'] ?>">
