686338c92e5d5d9694ee1248bde954f194556940
Alex Januar <74190057+alexanderjanuar@users.noreply.github.com>
2022-11-12 14:56:44 +0800

    Riwayat

---
+++ riwayat.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/riwayat.css?v=<?php echo time(); ?>">
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar">
+            <img src="resources/assets/logo.png" class="logo">
+            <h1>Playstation Game Store</h1>
+            <nav>
+                <ul id="menuList">
+                    <li><a href="index.php">Beranda</a></li>
+                    <li><a href="catalog.php">Catalog</a></li>
+                    <li><a href="catalog.php">Profile</a></li>
+                    <li>
+                        <label>
+                            <input type="checkbox" class="checkbox" id="modegelap">
+                            <span class="check"></span>
+                        </label>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <section>
+            <!--for demo wrap-->
+            <h1>RIWAYAT PEMESANAN</h1>
+            <div class="tbl-header">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Produk</th>
+                            <th>Jumlah</th>
+                            <th>Total Harga</th>
+                            <th>Tanggal</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="tbl-content">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Playstation 5 </td>
+                            <td>1</td>
+                            <td>Rp. 5.000.000</td>
+                            <td>12-01-2022</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+
+
+      
+
+
+        <script src="resources/js/payment.js"></script>
+
+</body>
+
+
+
+</html>
