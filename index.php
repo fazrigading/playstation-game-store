e680b171464f28efa5d13865c2bc90b9ab690296
Fazri Gading <fazrigading@gmail.com>
2022-11-13 12:47:55 +0800

    fixed index navbar

--- index.php
+++ index.php
@@ -15,36 +15,36 @@ require 'config.php';
 </head>
 
 <body>
-  <div class="container">
-    <div class="navbar">
-      <img src="./resources/assets/logo.png" class="logo">
-      <nav>
-        <ul id="menuList">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="catalog.php">Catalog</a></li>
-          <?php
-          if (isset($_SESSION["loginAdmin"])) {
-            echo "<li><a href='admin/products/'>Dashboard</a></li>";
-          } else if (!isset($_SESSION["loginUser"]) && !isset($_SESSION["loginAdmin"])) {
-            echo "<li><a href='auth.php'>Login</a></li>";
-          }
-          if (isset($_SESSION["loginUser"]) || isset($_SESSION["loginAdmin"])) {
-            echo "<li><a href='payment/'>Cart</a></li>";
-            echo "<li><a href='riwayat.php'>Riwayat</a></li>";
-            echo "<li><a href='profile.php'>Profile</a></li>";
-            echo "<li><a href='logout.php'>Logout</a></li>";
-          }
-          ?>
-          <li>
-            <label>
-              <input type="checkbox" class="checkbox" id="modegelap">
-              <span class="check"></span>
-            </label>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <div class="navbar">
+    <img src="./resources/assets/logo.png" class="logo">
+    <nav>
+      <ul id="menuList">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="catalog.php">Catalog</a></li>
+        <?php
+        if (isset($_SESSION["loginAdmin"])) {
+          echo "<li><a href='admin/products/'>Dashboard</a></li>";
+        } else if (!isset($_SESSION["loginUser"]) && !isset($_SESSION["loginAdmin"])) {
+          echo "<li><a href='auth.php'>Login</a></li>";
+        }
+        if (isset($_SESSION["loginUser"]) || isset($_SESSION["loginAdmin"])) {
+          echo "<li><a href='payment/'>Cart</a></li>";
+          echo "<li><a href='riwayat.php'>Riwayat</a></li>";
+          echo "<li><a href='profile.php'>Profile</a></li>";
+          echo "<li><a href='logout.php'>Logout</a></li>";
+        }
+        ?>
+        <li>
+          <label>
+            <input type="checkbox" class="checkbox" id="modegelap">
+            <span class="check"></span>
+          </label>
+        </li>
+      </ul>
+    </nav>
+  </div>
 
+  <div class="container">
     <div class="row">
       <div class="col-1">
         <h2>PS4 V2<br>Dualshock 4</h2>
