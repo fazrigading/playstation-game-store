8d81f45c0f0ce8bd2943174e015e7d7c12264ad7
Fazri Gading <fazrigading@gmail.com>
2022-11-14 07:36:15 +0800

    add pagination to accessories and games page

--- games.php
+++ games.php
@@ -128,6 +128,17 @@ require 'config.php';
             <p>Rp499.000</p>
           </div>
         </div>
+
+        <div class="pagination">
+          <a href="#">&laquo;</a>
+          <a class="active" href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">6</a>
+          <a href="#">&raquo;</a>
+        </div>
       </div>
     </div>
   </div>
